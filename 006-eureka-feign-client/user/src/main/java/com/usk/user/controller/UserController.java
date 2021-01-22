e8f79db5ffb444fee949a64fa9abb9c7306a47e7
Santhosh Uppari <uppari.santhosh.kumar@gmail.com>
2021-01-22 13:26:46 +0530

    Updated

---
+++ 006-eureka-feign-client/user/src/main/java/com/usk/user/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.usk.user.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usk.user.dto.OrderResponse;
+import com.usk.user.dto.User;
+import com.usk.user.dto.UserOrdersResponse;
+import com.usk.user.feignclient.OrderClient;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	OrderClient orderClient;
+
+	static Map<String, User> userDetails = new HashMap<>();
+	
+	static {
+
+		User user = new User();
+		user.setUserId("101");
+		user.setUserName("Test1");
+		
+		userDetails.put("101", user);
+
+		user = new User();
+		user.setUserId("201");
+		user.setUserName("Test2");
+		
+		userDetails.put("201", user);
+		
+	}
+	
+	@GetMapping("/{userId}")
+	public UserOrdersResponse getUserOrders(@PathVariable String userId) {
+		User user = userDetails.get(userId);
+		
+		OrderResponse orderResponse = new OrderResponse();
+		orderResponse.setId(103);
+		orderResponse.setDes("new order");
+		
+		
+		List<OrderResponse> orders = orderClient.getAllByPostReqBody(userId, orderResponse);
+		
+		UserOrdersResponse userOrdersResponse = new UserOrdersResponse();
+		userOrdersResponse.setUser(user);
+		userOrdersResponse.setOrders(orders);
+		return userOrdersResponse;
+		
+	}
+}
