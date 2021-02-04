94772d8d7c1454980b86588e0b89edd96f026867
Santhosh Uppari <uppari.santhosh.kumar@gmail.com>
2021-02-04 09:03:58 +0530

    Updated

--- springboot-file/src/main/java/com/usk/springbootfile/controller/FileUploadController.java
+++ springboot-file/src/main/java/com/usk/springbootfile/controller/FileUploadController.java
@@ -6,12 +6,14 @@ import java.io.IOException;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.usk.springbootfile.constants.FileConstants;
+import com.usk.springbootfile.dto.RequestDto;
 
 @RestController
 @RequestMapping("/file")
@@ -22,17 +24,32 @@ public class FileUploadController {
 		if (file.isEmpty()) {
 			return "Please select the file.";
 		}
-		File newFile = new File(FileConstants.UPLOADED_FOLDER + file.getOriginalFilename());
+		saveFile(file);
+		return "File is uploaded successfully";
+	}
+
+	@PostMapping(value = "/uploads", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public String uploadFiles(@RequestBody RequestDto requestDto,  @RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2) {
+		if (file1.isEmpty()) {
+			return "Please select the file.";
+		}
+		System.out.println("=====>> "+requestDto.getName());
+		System.out.println("=====>> "+requestDto.getDesc());
+		saveFile(file1);
+		saveFile(file2);
+		return "File is uploaded successfully";
+	}
+
+	private void saveFile(MultipartFile file1) {
+		File newFile = new File(FileConstants.UPLOADED_FOLDER + file1.getOriginalFilename());
 		System.out.println("newFile ==========>> " + newFile);
 		try {
 			newFile.createNewFile();
 			FileOutputStream fout = new FileOutputStream(newFile);
-			fout.write(file.getBytes());
+			fout.write(file1.getBytes());
 			fout.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return "Check your file and try again";
 		}
-		return "File is uploaded successfully";
 	}
 }
---
+++ springboot-file/src/main/java/com/usk/springbootfile/dto/RequestDto.java
@@ -0,0 +1,25 @@
+package com.usk.springbootfile.dto;
+
+public class RequestDto {
+	
+	private String name;
+	
+	private String desc;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+}
