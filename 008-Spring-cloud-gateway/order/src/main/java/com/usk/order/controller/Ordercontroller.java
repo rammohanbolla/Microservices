f769c78d561cf4d452b348254234ae1e9adcd6c1
Santhosh Uppari <uppari.santhosh.kumar@gmail.com>
2021-02-05 14:20:26 +0530

    Updated

---
+++ 008-Spring-cloud-gateway/order/src/main/java/com/usk/order/controller/Ordercontroller.java
@@ -0,0 +1,15 @@
+package com.usk.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class Ordercontroller {
+
+	@GetMapping("/test")
+	public String test() {
+		return "from order service";
+	}
+}
