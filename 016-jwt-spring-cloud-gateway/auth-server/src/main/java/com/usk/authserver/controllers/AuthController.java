a36872e0b68b3e58da86bff6f17aaeea8e8e7f7b
Santhosh-U <santhosh.u@ddistechnologies.com>
2022-02-22 09:58:17 +0530

    Updated

---
+++ 016-jwt-spring-cloud-gateway/auth-server/src/main/java/com/usk/authserver/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.usk.authserver.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usk.authserver.entities.AuthRequest;
+import com.usk.authserver.entities.AuthResponse;
+import com.usk.authserver.services.AuthService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(final AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.register(authRequest));
+    }
+
+}
